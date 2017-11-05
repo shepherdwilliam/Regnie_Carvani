fc6aeecae07429e6632afc454c64d4f85b5f9961
shepherdwilliam <shsoft.corp@gmail.com>
2017-11-05 14:21:44 +0900

    画面遷移プロトタイプ

--- Assets/Scripts/SceneManager/SceneManagerScript.cs
+++ Assets/Scripts/SceneManager/SceneManagerScript.cs
@@ -1,6 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public interface ICustomMessageTarget : IEventSystemHandler{
+	// メッセージングシステムを通して呼び出される関数
+	void Battle();
+	void Summon();
+}
 
 public class SceneManagerScript : MonoBehaviour {
 
@@ -12,5 +20,25 @@ public class SceneManagerScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	public void MessageBattle(){
+		Debug.Log ("Message 1 received");
+		SceneManager.LoadScene ("Battle");
+	}
+
+	public void MessageSummon(){
+		Debug.Log ("Message 2 received");
+		SceneManager.LoadScene ("Summon");
+	}
+
+	public void MessageDictionary(){
+		Debug.Log ("Message 3 received");
+		SceneManager.LoadScene ("Dictionary");
+	}
+
+	public void MessageStore(){
+		Debug.Log ("Message 4 received");
+		SceneManager.LoadScene ("Store");
 	}
 }
