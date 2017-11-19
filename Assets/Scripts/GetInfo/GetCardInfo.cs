e5b3e5dc4feaf267b1e39f41e597f36056c06702
shepherdwilliam <shsoft.corp@gmail.com>
2017-11-19 15:56:33 +0900

    1119 アップデート

--- Assets/Scripts/GetInfo/GetCardInfo.cs
+++ Assets/Scripts/GetInfo/GetCardInfo.cs
@@ -6,17 +6,19 @@ using UnityEngine.EventSystems;
 
 public class GetCardInfo : MonoBehaviour {
 
-	public TextAsset xmlTextAsset;
-	public XmlDocument document = new XmlDocument();
+	//public TextAsset xmlTextAsset;
+	//public XmlDocument document = new XmlDocument();
 
 	// Use this for initialization
 	void Start () {
+		/*
 		foreach( XmlElement element in document.DocumentElement ){
 			string text = element.InnerText;                         // 要素の内容
 			string attribute = element.GetAttribute( "attribute" );  // 属性
 			Debug.Log (text);
 			Debug.Log (attribute);
 		}
+		*/
 	}
 	
 	// Update is called once per frame
---
+++ Assets/Scripts/Welcome.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Welcome : MonoBehaviour {
+
+	/*
+	 * Welcome to REGNIE CARVANI!
+	 * Hitsujikai-P (twitter:@hitsujikaip)
+	 * ------------------------------------------------
+	 * Copyright 2017 Hitsujikai-P All Rights Reserved. 
+	 * 
+	 */
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
